91037b3bfeb6bb19020b1547f088c8dcb0a148d8
HarshTripathih <harshtripathih321@gmail.com>
2024-05-15 00:42:47 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3002/get-data');
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const lightThemeClass = {
+    backgroundColor: '#ffffff',
+    color: '#000000',
+  };
+
+  const darkThemeClass = {
+    backgroundColor: '#000000',
+    color: '#ffffff',
+  };
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={theme === 'light' ? lightThemeClass : darkThemeClass}>
+      <div className="d-flex justify-content-between">
+        <h1 className="text-center">HODLINFO</h1>
+        <button className="btn btn-primary" onClick={toggleTheme}>
+          Toggle Theme
+        </button>
+      </div>
+      <table
+        className={`table ${theme === 'light' ? 'table-light' : 'table-dark'} table-hover`}
+      >
+        <thead>
+          <tr>
+            <th className="text-danger">#</th>
+            <th className="text-danger">Name</th>
+            <th className="text-danger">Last Price</th>
+            <th className="text-danger">Buy Price</th>
+            <th className="text-danger">Sell Price</th>
+            <th className="text-danger">Volume</th>
+            <th className="text-danger">Base Unit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((crypto, index) => (
+            <tr key={crypto.id}>
+              <td>{index}</td>
+              <td>{crypto.name}</td>
+              <td>₹ {crypto.last}</td>
+              <td>₹ {crypto.buy}</td>
+              <td>₹ {crypto.sell}</td>
+              <td>{crypto.volume}</td>
+              <td>{crypto.base_unit}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
